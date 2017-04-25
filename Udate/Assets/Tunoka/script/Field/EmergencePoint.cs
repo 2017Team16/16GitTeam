097fb672f4a3db53bb6bf97366ff7c37049431c9
tunomizu <tumikihurai46@gmail.com>
2017-04-25 16:38:47 +0900

    敵のスポーン場所　Particle ステージの細かい焼成

---
+++ Udate/Assets/Tunoka/script/Field/EmergencePoint.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmergencePoint : MonoBehaviour {
+
+    [SerializeField, Header("敵の種類入れ")]
+    private GameObject[] _EnemyType;
+    [SerializeField, Header("出る敵の種類(_EnemyTypeの配列から選ぶ)")]
+    public int _Type = 0;
+    [SerializeField, Header("出る敵の数")]
+    public float _Amount = 1;
+    [SerializeField, Header("出る場所(子にある名前と同じ数値)")]
+    public float _Pointnum = 1;
+
+    [SerializeField, Header("出現用Particle(Dustsmoke)")]
+    public GameObject _Enemyspawner ;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+        //デバック用=======================================================================
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            spawner(0,1,1);
+        }
+        //=================================================================================
+
+    }
+    void spawner(int type , float amount , float pointnum)//出る敵の種類 出る敵の数 出る場所
+    {
+        _Type     = type;
+        _Amount   = amount;
+        _Pointnum = pointnum;
+
+
+        if (transform.childCount < _Pointnum || _Pointnum <= 0)//ポインち場所
+        {
+            print("そのポイントは存在しないよ"); 
+            return;
+        }
+
+        GameObject wreckClone = (GameObject)Instantiate
+           (_Enemyspawner, 
+            transform.FindChild(_Pointnum.ToString()).gameObject.transform.position, 
+            Quaternion.identity);
+        wreckClone.GetComponent<Enemyspawner>().OccurrenceSetting(_EnemyType[_Type], _Amount);
+    }
+}
---
+++ Udate/Assets/Tunoka/script/Field/Enemyspawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemyspawner : MonoBehaviour {
+
+    private GameObject _Type ;
+    private float _amount = 1;
+
+   
+
+    private ParticleSystem _particleSystem;
+
+    void Start () {
+        _particleSystem = transform.GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_particleSystem.IsAlive())//Particleが終わったら敵を生成
+        {
+            EnemyOccurrence();
+            Destroy(gameObject);
+        }
+      
+    }
+    void EnemyOccurrence()
+    {
+        GameObject wreckClone = (GameObject)Instantiate(_Type, transform.position, Quaternion.identity);
+    }
+    public void OccurrenceSetting(GameObject tupe , float amount)
+    {
+        _Type = tupe;
+        _amount = amount;
+    }
+
+}
