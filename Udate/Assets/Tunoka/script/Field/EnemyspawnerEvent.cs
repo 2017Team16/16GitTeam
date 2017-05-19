a5850b549291696b84dda971a40dea631d231f54
tunomizu <tumikihurai46@gmail.com>
2017-05-19 10:09:33 +0900

    エネミースポーン関係下準備

---
+++ Udate/Assets/Tunoka/script/Field/EnemyspawnerEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyspawnerEvent : MonoBehaviour {
+
+
+    [SerializeField, Header("Fieldにいる敵の数")]
+    private float _CountEnemy;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
