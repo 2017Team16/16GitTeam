562fca5a6176c980e8b3a74d46c5ba5c22c58556
RedTheCreator <623695056@qq.com>
2017-06-23 13:41:21 +0900

    5.4.1f1バージョンのGakenフォルダ再作成、とりあえずクレジットシーンの作成

---
+++ Udate/Assets/Gaken/Credit/CreditScroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditScroller : MonoBehaviour {
+
+    [Header("スクロール速度")]
+    public float moveSpeedY = 30.0f;
+    [Header("終わり時間線")]
+    public float overLine = 1600.0f;
+    [Header("フェイドコントローラ")]
+    public GameObject fadeObject;
+    private FadeScript fadeController;
+    private Vector2 position = Vector2.zero;
+
+    private bool isAnyKeyDown;
+
+	// Use this for initialization
+	void Start () {
+        isAnyKeyDown = false;
+        position = transform.position;
+        fadeController = fadeObject.GetComponent<FadeScript>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        position.y += moveSpeedY * Time.deltaTime;
+        transform.position = position;
+        //Debug.Log(position.y);
+
+        if (Input.anyKeyDown) isAnyKeyDown = true;
+
+        if (transform.position.y >= overLine || isAnyKeyDown)
+        {
+            //まずフェイドアウトする
+            fadeController.FadeOut();
+        }
+
+        //フェイドアウト完了したら遷移へ
+        if (fadeController.Alpha>= 1.0f)
+        {
+            //タイトルシーンを読み込み
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
---
+++ Udate/Assets/Gaken/Credit/FadeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadeScript : MonoBehaviour {
+
+    public float speed = 0.01f;          //透明化の速さ
+    private float alpha;                 //A値を操作するための変数
+    private float red, green, blue;      //RGBを操作するための変数
+
+    // Use this for initialization
+    void Start () {
+        //Panelの色を取得
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void FadeOut()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alpha);
+        alpha += speed;
+    }
+
+    public float Alpha
+    {
+        get
+        {
+            return alpha;
+        }
+    }
+}
