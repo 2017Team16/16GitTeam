7d7657f486afd4c9c9e9f14592b8c0e22915fd28
TYasutake <kokoroyuzuka@yahoo.co.jp>
2017-07-11 16:24:38 +0900

    チュートリアル修正　それぞれのUIを説明するときに、前に来るように

---
+++ Udate/Assets/Yasutake/Script/Tutolial/TutorialPlayerUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialPlayerUI : MonoBehaviour {
+
+    private TutorialManager tMane;
+    public int[] myActiveNumber;
+    public int defaultRenNumber;
+    public int renNumber;
+
+	// Use this for initialization
+	void Start () {
+        tMane = GameObject.Find("TutorialManager").GetComponent<TutorialManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < myActiveNumber.Length; i++)
+        {
+            if(tMane.GetTutorialNumber() == myActiveNumber[i])
+            {
+                transform.SetSiblingIndex(renNumber);
+                return;
+            }
+            else
+            {
+                transform.SetSiblingIndex(defaultRenNumber);
+            }
+        }
+	}
+}
