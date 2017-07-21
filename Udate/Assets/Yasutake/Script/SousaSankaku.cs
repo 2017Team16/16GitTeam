975b43affe73cd6ca61c3a1ae53de72bc3205c4c
TYasutake <kokoroyuzuka@yahoo.co.jp>
2017-07-21 10:52:54 +0900

    メインステージに壁に当てた時のエフェクト、操作説明時の動き追加　　ケージの壁にはエフェクトいらないかも

---
+++ Udate/Assets/Yasutake/Script/SousaSankaku.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SousaSankaku : MonoBehaviour {
+
+    public float speed;//往復速度
+    public float range;//移動範囲
+
+    private Vector3 origin;//起点（移動の中心）
+    private float myTime = 0.0f;
+    
+    // Use this for initialization
+    void Start()
+    {
+        origin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTime += 1 / 60.0f;
+        transform.position =
+                origin + Vector3.up * Mathf.Sin(myTime * speed) * range;
+    }
+}
